bde7dc94387b4f6e25edc1710601672893fc6fce
ChrisSQR-alpha <64338646+ChrisSQR-alpha@users.noreply.github.com>
2020-06-16 17:44:12 +0800

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+/*$('.ham-icon').click(function(e) {
+  e.preventDefault();
+  $('.dropdown').toggleClass('active')
+})*/
+
+
+// 隱藏所有的 li 內文
+$('.answer').hide();
+// 預設讓第一個 li 加上 active
+$('.cardItem').eq(0).addClass('active');
+// 預設讓第一個 li 內文 顯示
+$('.answer').eq(0).show();
+
+// 點擊 li
+$('.cardItem').click(function (e) {
+  // 取消預設 event 事件
+  e.preventDefault(); 
+  // 加上 active 屬性，並把其他有 active 的移除
+  $(this).toggleClass('active').siblings().removeClass('active');
+  // 讓自己的 li body 切換收合
+  $(this).children().next().slideToggle();
+  // 讓其他 li body 收合
+  $(this).siblings().children().next().slideUp();
+})
+
+});
